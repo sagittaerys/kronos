1fb930bd600625626c05228ffc2cea23404980ed
Olamilekan Aremu <aremu0235@gmail.com>
2026-05-10 17:46:15 +0100

    feat(WIP): initialized kronos et project set-up

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { MotionPlugin } from '@vueuse/motion'
+import App from './App.vue'
+import './style.css'
+
+const app = createApp(App)
+
+app.use(createPinia())   
+app.use(MotionPlugin)   
+
+app.mount('#app')
